cdb7dfedf746087dffef7a4000028ada9298674b
Leona-cloud <chisomokafor038@gmail.com>
2022-07-26 20:11:10 +0100

    chore: complete notes endpoint

--- dist/routes/notes.js
+++ dist/routes/notes.js
@@ -14,19 +14,70 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const notes_1 = __importDefault(require("../models/notes"));
+const auth_1 = __importDefault(require("../middleware/auth"));
 const lodash_1 = __importDefault(require("lodash"));
 const NoteRouter = (0, express_1.Router)();
-NoteRouter.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+NoteRouter.get('/notes', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const notes = yield notes_1.default.find().sort('title');
     res.status(200).json(Object.assign({ success: true, message: "notes retrieved successfully" }, notes));
 }));
-NoteRouter.post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+NoteRouter.post('/create', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const notes = new notes_1.default(lodash_1.default.pick(req.body, ["title", "body", "author"]));
     const result = yield notes.save();
     res.status(200).json({
         success: true,
         message: "notes saved successfully",
+        notes: result
+    });
+}));
+NoteRouter.get('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const notes = yield notes_1.default.findById({ _id: req.params.id });
+    if (notes) {
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        });
+    }
+    ;
+    res.json({
+        success: true,
+        message: "notes fetched successfully",
         notes
     });
+}));
+NoteRouter.put(":/id", auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let note = yield notes_1.default.findByIdAndUpdate({
+        _id: req.params.id,
+        title: req.body.title, body: req.body.body,
+        new: true
+    });
+    if (!note) {
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        });
+    }
+    else {
+        return res.json({
+            success: true,
+            message: "notes updated successfully",
+            note
+        });
+    }
+}));
+NoteRouter.delete('/:id', auth_1.default, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    let note = yield notes_1.default.findByIdAndRemove({ _id: req.params.id });
+    if (!note) {
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        });
+    }
+    else {
+        return res.json({
+            success: true,
+            message: "note deleted successfully"
+        });
+    }
 }));
 exports.default = NoteRouter;
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -5,7 +5,7 @@ import _  from 'lodash'
 
 const NoteRouter: Router = Router();
 
-NoteRouter.get('/', async(req, res) => {
+NoteRouter.get('/notes', Auth, async(req: Request, res: Response) => {
     const notes = await Note.find().sort('title');
     res.status(200).json({
         success: true,
@@ -14,15 +14,65 @@ NoteRouter.get('/', async(req, res) => {
     })
 });
 
-NoteRouter.post('/', async(req, res) => {
+NoteRouter.post('/create', Auth, async(req: Request, res: Response) => {
     const notes = new Note(_.pick(req.body, [ "title", "body", "author"]));
     const result = await notes.save();
     res.status(200).json({
         success: true,
         message: "notes saved successfully",
+        notes: result
+    })
+});
+
+NoteRouter.get('/:id', Auth, async(req: Request, res: Response) => {
+    const notes = await Note.findById({_id: req.params.id});
+    if(notes){ 
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        })
+    };
+    res.json({
+        success: true,
+        message: "notes fetched successfully",
         notes
     })
 });
 
+NoteRouter.put(":/id", Auth, async (req: Request, res: Response) => {
+    let note = await Note.findByIdAndUpdate({
+        _id: req.params.id,
+         title: req.body.title, body: req.body.body ,
+         new: true
+    });
+    if(!note){
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        })
+    }else{
+        return res.json({
+            success: true,
+            message: "notes updated successfully",
+            note
+        })
+    }
+});
+
+
+NoteRouter.delete('/:id', Auth, async (req: Request, res: Response) => {
+    let note = await Note.findByIdAndRemove({_id: req.params.id});
+    if (!note){
+        return res.status(400).json({
+            success: false,
+            message: "note not found!!!"
+        })
+    }else{
+        return res.json({
+            success: true,
+            message: "note deleted successfully"
+        })
+    }
+});
 
 export default NoteRouter;
